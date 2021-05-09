6e7281ec6a71386f64790f01a10f6d5ad1e2ae7c
jackhong12 <jackhuang1205@gmail.com>
2021-05-09 22:54:00 +0800

    Add YCM dependencies

--- install.sh
+++ install.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-root_conf="${HOME}/.vimrc"
-
-if [ -z "$1" ]
-then
-	path="${HOME}/vim_setting"
-	rm -rf $path
-	git clone https://github.com/gpakosz/.tmux.git $path > /dev/null 2>&1 || exit 1
-else
-	path="${1}"
-fi
-
-set -x
 sudo apt-get install vim -y
+sudo apt-get install curl -y
+sudo apt-get install git -y
+
 # plugin manager
-git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/vundle
+
+# color scheme setting
+mkdir -p ${HOME}/.vim/colors
+cp ./color/molokai.vim ${HOME}/.vim/colors/
 
 # for Plugin 'godlygeek/tabular'
 sudo apt-get install ctags -y
 
 # for Plugin 'suan/vim-instant-markdown'
-sudo apt install npm -y
-sudo npm -g install instant-markdown-d -y
+#sudo apt install npm -y
+#sudo npm -g install instant-markdown-d -y
 
 # for Plugin 'vim-airline/vim-airline'
 sudo apt-get install fonts-powerline -y
@@ -30,11 +25,13 @@ sudo apt-get install fonts-powerline -y
 #sudo apt-get install vim-gnome -y
 sudo apt-get install vim-gtk -y
 
-# color scheme setting
-mkdir -p ~/.vim/colors
-cp ./color/molokai.vim ~/.vim/colors/
 
 # configuration file
 # .vimrc
-echo "source ${path}/setting.vim" > $root_conf
+echo "source $(pwd)/setting.vim" > ${HOME}/.vimrc
 vim +PluginInstall +qall
+
+# YCM
+sudo apt-get install build-essential cmake vim-nox python3-dev -y
+sudo apt-get install mono-complete golang nodejs default-jdk npm -y
+#cd ${HOME}/.vim/bundle/YouCompleteMe && python3 install.py --all
