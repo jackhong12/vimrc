268760cc341c5d952bc8e6000526afd04d4d5b8b
jackh <jackhuang1205@gmail.com>
2023-03-02 02:28:12 +0800

    Add molokai color scheme

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 mkdir ~/.vim/plugin -p
-
 for f in `pwd`/plugin/*.vim; do
     ln -sf $f ~/.vim/plugin 
-    echo $f
+done
+
+mkdir ~/.vim/colors -p
+for f in `pwd`/colors/*.vim; do
+    ln -sf $f ~/.vim/colors
 done
 
 vim +PluginInstall +qall
