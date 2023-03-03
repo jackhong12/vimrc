d96ab1d25ddb0bb7c72e88dedc7bd8a29653ea00
jackhong12 <jackhuang1205@gmail.com>
2023-03-03 11:36:55 +0800

    Install requirements

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+check_install () {
+    for exe in "$@"; do
+        if ! command -v $exe &> /dev/null; then
+            sudo apt-get install -y $exe
+        fi
+    done
+}
+
+check_install vim curl git
+
+# use vundle to manage plugins
+vundlepath=${HOME}/.vim/bundle/vundle
+if [ ! -d $vundlepath ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $vundlepath
+fi
+
+ln -sf `pwd`/.vimrc ~/.vimrc
 mkdir ~/.vim/plugin -p
 for f in `pwd`/plugin/*.vim; do
     ln -sf $f ~/.vim/plugin 
 done
-
 mkdir ~/.vim/colors -p
 for f in `pwd`/colors/*.vim; do
     ln -sf $f ~/.vim/colors
