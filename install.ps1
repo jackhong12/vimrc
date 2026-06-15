403c6b70ea98c27ee28871674ce0d10ad9152c3c
Jack Huang <jackhuang1205@gmail.com>
2026-06-15 15:19:10 +0800

    Fix install.ps1 and show more information

--- install.ps1
+++ install.ps1
@@ -1,5 +1,7 @@
 # A script to install vim pulgins.
 
+Write-Host "[INFO] Home path: $env:HOME"
+
 function run {
     param(
         [string]$command,
@@ -20,7 +22,7 @@ winget list --id vim.vim > $null 2>&1
 if ($LASTEXITCODE -ne 0) {
     run winget install vim.vim
 }
-$vim = (Get-ChildItem "C:\Program Files" -Recurse -Filter vim.exe -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
+$vim = (Get-ChildItem "C:\Program Files\Vim" -Recurse -Filter vim.exe -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
 Write-Host "[INFO] vim path: $vim"
 
 # Install vim plugin manager
@@ -46,7 +48,18 @@ if (-not(Test-Path "$HOME\.cache\vim\undo")) {
 }
 
 # Copy color scheme
-run cp colors/molokai.vim "$HOME\vimfiles\colors"
+if (-not(Test-Path "$HOME\vimfiles\colors")) {
+   run mkdir "$HOME\vimfiles\colors"
+}
+run cp colors/molokai.vim "$HOME\vimfiles\colors\molokai.vim"
 
 run cp windows.vimrc "$HOME/_vimrc"
-run vim +PluginInstall +qall
+run $vim +PluginInstall +qall
+
+$vimrc_path = (vim --version | Select-String "  user vimrc file").ToString().Split(":")[1].Trim()
+Write-Host ""
+Write-Host ""
+Write-Host "Summary:" -ForegroundColor Green
+Write-Host "    vim path:   $vim"
+Write-Host "    user path:  $env:HOME"
+Write-Host "    vimrc path: $vimrc_path"
